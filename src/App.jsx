e93937bc0dabd1c828605d88c6796b4c26a53eff
Diego_kahuaski <kahuaski99@gmail.com>
2023-04-07 12:06:19 -0500

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
+import Weather from './componets/Weather'
+import Loader from './componets/Loader'
+
+function App() {
+  const [weather, setWeather] = useState()
+  const [coords, setCoords] = useState()
+  const [temp, settemp] = useState()
+  const event = (pos) => {
+    console.log(pos)
+    const currentCoords = {
+      lat: pos.coords.latitude,
+      longt: pos.coords.longitude
+    }
+    setCoords(currentCoords)
+  }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(event)
+  }, [])
+  useEffect(() => {
+    if (coords) {
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.longt}&appid=87b3890c51e780604422b8ff2c22741c`
+      axios
+        .get(URL)
+        .then((res) => {setWeather(res.data)
+        const celcius= (res.data.main.temp - 267.15).toFixed(1)
+        const fahrenheit=(celcius*(9/5)+32).toFixed(1)
+        const ntemp= {
+        celcius,fahrenheit
+        }
+        console.log(ntemp)
+        settemp(ntemp)
+        })
+        .catch((error) => console.log(error))
+    }
+
+  }, [coords])
+
+
+  return (
+    <div className="App grid place-content-center min-h-screen bg-[url('public/clima.jpeg')] bg-cover px-2">
+      {
+        weather ? (
+
+          <Weather weather={weather} temp ={temp} />
+        ) :
+          (
+            <Loader />
+          )
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ src/componets/Weather.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Weather = ({ weather, temp }) => {
+  const [isCelcius, setisCelcius] = useState(true)
+  console.log(weather)
+  //const Temp=wather.temp
+  const changeUnitTemp = () => setisCelcius(!isCelcius)
+  return (
+    
+     <section className='text-xl'>
+    <h2 className='text-center nb-4 font-bold '>{weather.sys.country} </h2>
+      <h2 className='text-center nb-4 font-bold' >{weather.name}</h2>
+      
+      <section className='grid gap-4 sm:grid-cols-2'>
+      
+      <article className="bg-slate-300/70 rounded-3xl grid grid-cols-2 justify-items-center items-center py-2">
+        <h3 className='capitalize col-start-1 col-end-3'>{weather.weather[0].description}</h3>
+
+        <h3 className='text-45px'>{isCelcius ? `${temp.celcius}  °C` : `${temp.fahrenheit}°F`} </h3>
+        <div>
+
+          <img src={` https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} alt="" />
+
+        </div>
+
+
+      </article>
+
+
+      <article className="bg-slate-300/70 rounded-3xl grid grid-cols-3 justify-items-center items-center py-2 sm:grid-cols-1 " >
+        <div className='flex text-sm justify-center items-center'>
+          <div>
+            <img src="public/windy.png" alt="" />
+          </div>
+          <h5>{weather.wind.speed}m/s </h5>
+        </div>
+
+        <div className='flex text-sm justify-center items-center'>
+          <div>
+            <img src="public/rain.png" alt="" />
+          </div>
+          <h5>{weather.main.humidity} % </h5>
+        </div>
+
+        <div className='flex text-sm justify-center items-center'>
+          <div>
+            <img src="public/tabler_arrow-wave-right-down.png " alt="" />
+          </div>
+          <h5>{weather.main.pressure}HPA </h5>
+        </div>
+      </article>
+
+      </section>
+      <button onClick={changeUnitTemp} className='bg-blue-500 py-2 px-4 rounded-full  hover:bg-blue-800 duration-200 text-sm block mx-auto nt-4 text-white font-bold'>Cambiar "C/F"</button>
+    </section>
+
+  )
+}
+
+export default Weather
